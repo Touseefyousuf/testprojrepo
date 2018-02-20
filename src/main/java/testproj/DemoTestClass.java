2158e7ca31fadd050134569c6978065ddb817004
I20030 <I20030@IDC-LT-I20030.mchp-main.com>
2018-02-20 08:30:39 +0530

    added the another print statement

--- src/main/java/testproj/DemoTestClass.java
+++ src/main/java/testproj/DemoTestClass.java
@@ -3,6 +3,7 @@ package testproj;
 public class DemoTestClass {
 
 	public static void main(String[] args) {
+		System.out.println("added other statement");
 		System.out.println("first statement for commit");
 	}
 
